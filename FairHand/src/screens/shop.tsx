a4041706e04d4ff25db5c5c7f47a6dbe6cd1052f
Amaia <amaia.semper@gmail.com>
2021-03-14 11:25:16 +0100

    Add new in section to Shop Details Add links to products

--- FairHand/src/screens/shop.tsx
+++ FairHand/src/screens/shop.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image, TouchableOpacity, Linking } from 'react-native'
-import { COLOR, SIZES, DATA } from '../../constants/'
+import { StyleSheet, Text, View, Image, Linking, FlatList } from 'react-native'
+import { COLOR, SIZES, DATA } from '../../constants'
 import { Ionicons, AntDesign } from '@expo/vector-icons'
 import renderHeader from './Header'
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler'
 
 const styles = StyleSheet.create({
   container: {
@@ -77,6 +78,58 @@ const styles = StyleSheet.create({
     fontSize: 30,
     color: COLOR.black,
     marginLeft: 20
+  },
+  newInContainer: {
+    marginLeft: 20
+  },
+  newInTitle: {
+    fontSize: SIZES.h2,
+    marginRight: 20,
+    fontWeight: '700',
+    color: COLOR.black,
+    borderBottomWidth: 1,
+    borderBottomColor: COLOR.black
+  },
+  newInItem: {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    width: 160,
+    height: 240,
+    marginTop: 10
+  },
+  newInImage: {
+    height: 150,
+    width: 150,
+    resizeMode: 'cover',
+    borderRadius: SIZES.cardRadius
+  },
+  newInInfo: {
+    marginTop: 5,
+    width: 150,
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start'
+  },
+  productName: {
+    fontSize: SIZES.p12,
+    fontWeight: '700'
+  },
+  price: {
+    fontSize: SIZES.p12,
+    color: COLOR.grey
+  },
+  newInbutton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 5,
+    width: 120,
+    height: 25,
+    backgroundColor: COLOR.orange,
+    borderRadius: SIZES.buttonRadius
+  },
+  buttonText: {
+    color: COLOR.white,
+    fontSize: SIZES.p12
   }
 
 })
@@ -84,6 +137,24 @@ const styles = StyleSheet.create({
 const data = DATA[0]
 
 const Shop = ({ data: any, navigation }: any) => {
+  const renderItem = ({ item }: any) => (
+    <View style={styles.newInItem}>
+        <Image
+          style = {styles.newInImage}
+          source={{ uri: item.productImage }}
+        ></Image>
+        <View style={styles.newInInfo}>
+          <Text style={styles.productName}>{item.productName}</Text>
+          <Text style={styles.price}>{item.price} €</Text>
+          <TouchableOpacity
+          onPress= {() => { Linking.openURL(item.url) }}
+          style={styles.newInbutton}>
+            <Text style={styles.buttonText}>See on the website</Text>
+          </TouchableOpacity>
+        </View>
+    </View>
+  )
+
   return (
         <View style = {styles.container}>
           {renderHeader()}
@@ -123,6 +194,15 @@ const Shop = ({ data: any, navigation }: any) => {
                   <AntDesign style={styles.phoneHeart} name="hearto" size={24} color="black" />
                 </TouchableOpacity>
               </View>
+            </View>
+            <View style={styles.newInContainer}>
+            <Text style={styles.newInTitle}>New in</Text>
+                <FlatList
+                  horizontal={true}
+                  data={data.NewIn}
+                  renderItem={renderItem}
+                  keyExtractor={item => item.productName}
+                ></FlatList>
             </View>
           </View>
         </View>
